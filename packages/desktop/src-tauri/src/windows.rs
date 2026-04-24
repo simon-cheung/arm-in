ea739ca7cb0d509da20dec601e1e3d6182b61b83
simon-cheung <vinrougeed@hotmail.com>
2026-04-24 11:26:13 +0800

    desktop title

--- packages/desktop/src-tauri/src/windows.rs
+++ packages/desktop/src-tauri/src/windows.rs
@@ -53,7 +53,7 @@ impl MainWindow {
             app,
             decorations,
         )
-        .title("OpenCode")
+        .title( option_env!("OPENCODE_DESKTOP_TITLE").unwrap_or("OpenCode"))
         .disable_drag_drop_handler()
         .zoom_hotkeys_enabled(false)
         .visible(true)
